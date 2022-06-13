285c5d413458ef8f71307a879f6d63d4af1fcccc
Boris Arko <trinajstica@users.noreply.github.com>
2022-06-13 07:33:43 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+# 
+# sudo dnf install ruby-devel gcc make rpm-build libffi-devel
+# sudo gem install --no-document fpm
+#
+wget -c https://download.mobirise.com/beta/mobirise5beta-setup.deb
+sudo fpm --no-depends -s deb -t rpm mobirise5beta-setup.deb
+sudo dnf install mobirise*.rpm

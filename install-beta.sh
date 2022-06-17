fc4364cba72ad9f7edb7a332f60c1966fcef397d
Boris Arko <trinajstica@users.noreply.github.com>
2022-06-17 16:03:19 +0200

    popravki

--- install-beta.sh
+++ install-beta.sh
@@ -3,6 +3,8 @@
 # sudo dnf install ruby-devel gcc make rpm-build libffi-devel
 # sudo gem install --no-document fpm
 #
+sudo rm *.deb *.rpm 2> /dev/null
 wget -c https://download.mobirise.com/beta/mobirise5beta-setup.deb
 sudo fpm --no-depends -s deb -t rpm mobirise5beta-setup.deb
 sudo dnf install mobirise*.rpm
+
--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # sudo dnf install ruby-devel gcc make rpm-build libffi-devel
 # sudo gem install --no-document fpm
 #
+sudo rm *.deb *.rpm 2> /dev/null
 wget -c https://download.mobirise.com/MobiriseSetup.deb
 sudo fpm --no-depends -s deb -t rpm MobiriseSetup.deb
 sudo dnf install mobirise*.rpm
+
